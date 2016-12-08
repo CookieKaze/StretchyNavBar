0ad24d92d59a6a3051761286126ea97718d5577a
Erin Luu <luuerin@gmail.com>
2016-12-08 10:35:06 -0500

    Initial Commit

---
+++ StretchyNavBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchyNavBar
+//
+//  Created by Erin Luu on 2016-12-08.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
